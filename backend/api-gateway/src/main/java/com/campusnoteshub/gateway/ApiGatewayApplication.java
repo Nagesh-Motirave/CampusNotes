1707a4dfd8619b5b01a664de1437b2ca4013836b
Nagesh-Motirave <nageshmotirave3@gmail.com>
2026-08-06 21:56:41 +0530

    login problem solved

--- backend/api-gateway/src/main/java/com/campusnoteshub/gateway/ApiGatewayApplication.java
+++ backend/api-gateway/src/main/java/com/campusnoteshub/gateway/ApiGatewayApplication.java
@@ -2,14 +2,22 @@ package com.campusnoteshub.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * API Gateway application entry point.
  * Routes requests to downstream microservices.
  */
 @SpringBootApplication
+@RestController
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "API Gateway is running!";
     }
 }
